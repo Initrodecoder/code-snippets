52642b4e1be91fdea35bc20220c0c0f0640f12a8
Sean Moore <seanmoore1976@gmail.com>
2019-03-07 17:01:51 -0800

    move code from freecodecamp exersice into file.

---
+++ find-longest-word-length.js
@@ -0,0 +1,20 @@
+const findLongestWordLength = (str) => {
+  let a = str.split(" ");
+  let lw = 0;
+  a.forEach((elm,idx,self)=>{
+    const hasNext = self[idx+1];
+    if (hasNext) {
+      let nl = self[idx+1].length;
+      if (nl > elm.length) {
+        if (nl > lw) {
+          lw = nl;
+        }
+      }
+    }
+  })
+  return lw;
+}
+
+console.log(
+    findLongestWordLength("The quick brown fox jumped over the lazy dog");
+)
